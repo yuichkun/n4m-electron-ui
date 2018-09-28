c9d7d657e6b28ddbd6e72975eb0221c98290c7d7
Yuichi Yogo <yuichkun@gmail.com>
2018-09-28 20:38:29 +0900

    refactor

--- electron/main.js
+++ electron/main.js
@@ -1,14 +1,22 @@
+
+// Because this file is specified as the `main` in package.json
+// this file will be the entry point for Electron App
+// when `npm start` is called.
+
 const { app, BrowserWindow } = require('electron');
 
 app.on('ready', function() {
-  console.log('app reeeeeeeead');
+  console.log('Electron has started');
+  // Launch a window and load index.html
   let window = new BrowserWindow({width: 800, height: 600});
   window.loadFile('./index.html');
-  window.webContents.openDevTools();
 
   window.on('closed', function() {
     window = null;
   });
+
+  // For Development Only
+  window.webContents.openDevTools();
 });
 
 app.on('window-all-closed', function() {
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+
+// This file is the single-entrypoint for the Max patch.
+// When this file is executed via `node.script start` in the Max patch,
+// this program will launch an Electron app as a child process, and connect to it by socket.io
+
 const MaxAPI = require('max-api');
 const io = require('socket.io')();
 const electron = require('electron');
@@ -13,16 +18,11 @@ io.on('connection', function (client) {
 });
 io.listen(3000);
 
+
+// This will ensure that when this parent process is killed in maxpat (either by `node.script stop` or Max is shutdown for some reason),
+// it will terminate the child process, the Electron app.
 process.on('exit', () => {
   child.kill();
 });
 
-
 console.log('hello');
-
-function hoge() {
-  MaxAPI.outlet(Math.random());
-}
-
-// setInterval(hoge, 300);
-
