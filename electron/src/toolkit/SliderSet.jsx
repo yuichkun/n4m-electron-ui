b842fbb765f7139c224e5fe9c2076440f87b1692
Yuichi Yogo <yuichkun@gmail.com>
2018-09-29 14:23:41 +0900

    add slider sets

---
+++ electron/src/toolkit/SliderSet.jsx
@@ -0,0 +1,47 @@
+import { Component } from 'react';
+import io from '../utils/io';
+import Slider from '@material-ui/lab/Slider';
+
+
+// {
+//   sliders: [
+//     {
+//       name: string,
+//       min: number,
+//       max: number,
+//     }
+//   ],
+//    defaults: {
+//     hoge: asdf,
+//         bar:adlfkj
+//     }
+// }
+
+export default class SliderSet extends Component {
+  constructor(props) {
+    super(props);
+    this.state = props.defaults;
+  }
+  render() {
+    return (
+      <div>
+        {this.renderSliders()}
+      </div>
+    );
+  }
+
+  renderSliders() {
+    const { sliders } = this.props;
+    return sliders.map((slider, i) => (
+      <Slider key={i} min={ slider.min } max={ slider.max } value={ this.state[slider.name] } onChange={(e,value)=>{this.handleChange(slider.name, value)}} />
+    )) 
+  }
+  handleChange(name, value) {
+    this.setState(
+      {
+        [name]: value
+      }
+    );
+    io.emit('dispatch', { [this.props.sliderSetName]: this.state });
+  }
+}
--- electron/src/toolkit/UIControls.jsx
+++ electron/src/toolkit/UIControls.jsx
@@ -1,5 +1,8 @@
 import ChannelsSlider from './ChannelsSlider.jsx';
 import ToggleButton from './ToggleButton.jsx';
+import SampleSourceSelector from './SampleSourceSelector.jsx';
+import SliderSet from './SliderSet.jsx';
+
 
 
 
@@ -9,5 +12,41 @@ export default () => (
     <ToggleButton />
     <h2>Channels</h2>
     <ChannelsSlider />
+    <h2>Sample Source</h2>
+    <SampleSourceSelector />
+    <h2>Spread Inclusive</h2>
+    <SliderSet
+      sliderSetName='spreadInclusive'
+      sliders={[
+        {
+          name: 'from',
+          min: 0,
+          max: 1.0
+        },
+        {
+          name: 'to',
+          min: 0,
+          max: 10
+        },
+      ]}
+      defaults={{from: 0, to: 1}}
+    />
+    <h2>Deviate</h2>
+    <SliderSet
+      sliderSetName='deviate'
+      sliders={[
+        {
+          name: 'from',
+          min: 0,
+          max: 1.0
+        },
+        {
+          name: 'to',
+          min: 0,
+          max: 1.0
+        },
+      ]}
+      defaults={{from: 0, to: 1}}
+    />
   </div>
 );
